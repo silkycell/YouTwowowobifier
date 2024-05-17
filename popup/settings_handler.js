4766263d5d2274af1474e04b7b4ebc62cc8fcb48
sylvia <75647306+silkycell@users.noreply.github.com>
2024-05-17 15:20:49 -0400

    move popup settings handling into its own file

---
+++ popup/settings_handler.js
@@ -0,0 +1,43 @@
+const enabledCheckbox = document.getElementById('checkbox_enabled')
+const pitchCheckbox = document.getElementById('checkbox_pitch')
+
+const speedNumber = document.getElementById('number_speed')
+const amplitudeNumber = document.getElementById('number_amplitude')
+const offsetNumber = document.getElementById('number_offset')
+
+const updateRateNumber = document.getElementById('number_updateRate')
+
+enabledCheckbox.addEventListener("click", sendSettingData);
+pitchCheckbox.addEventListener("click", sendSettingData);
+
+speedNumber.addEventListener("change", sendSettingData);
+amplitudeNumber.addEventListener("change", sendSettingData);
+offsetNumber.addEventListener("change", sendSettingData);
+
+updateRateNumber.addEventListener("change", sendSettingData);
+
+function sendSettingData() {
+    chrome.runtime.sendMessage({ event: "updateSettingsData", settings: getSettings() })
+}
+
+function getSettings() {
+    return {
+        isEnabled: enabledCheckbox.checked,
+        preservesPitch: pitchCheckbox.checked,
+        speed: parseFloat(speedNumber.value),
+        amplitude: parseFloat(amplitudeNumber.value),
+        offset: parseFloat(offsetNumber.value),
+        updateRate: parseFloat(updateRateNumber.value)
+    }
+}
+
+chrome.storage.local.get().then((savedSettings) => {
+    if (savedSettings.isEnabled !== undefined) enabledCheckbox.checked = savedSettings.isEnabled
+    if (savedSettings.preservesPitch !== undefined) pitchCheckbox.checked = savedSettings.preservesPitch
+
+    if (savedSettings.speed !== undefined) speedNumber.value = savedSettings.speed
+    if (savedSettings.amplitude !== undefined) amplitudeNumber.value = savedSettings.amplitude
+    if (savedSettings.offset !== undefined) offsetNumber.value = savedSettings.offset
+
+    if (savedSettings.updateRate !== undefined) updateRateNumber.value = savedSettings.updateRate
+})
