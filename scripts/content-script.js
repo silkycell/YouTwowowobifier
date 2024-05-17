6c093088c3e1bd826cb9ab69a2e9472439379cec
sylvia <75647306+silkycell@users.noreply.github.com>
2024-05-17 16:43:39 -0400

    remove default settings data from content-script.js

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,11 +1,5 @@
-let settings = {
-    isEnabled: false,
-    preservesPitch: false,
-    speed: 10,
-    amplitude: 0.5,
-    offset: 1,
-    updateRate: 10
-}
+let settings = undefined
+let updateIntervalID = undefined
 
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 
@@ -13,7 +7,7 @@ function updatePlaybackRate() {
     if (!settings.isEnabled) return
 
     var currentTime = Date.now()
-    
+
     var amplitude = clamp(settings.amplitude, 0, 7.95)
     var sineValue = (Math.sin((currentTime / 1000) * settings.speed) * amplitude) + amplitude + settings.offset + 0.1
     sineValue = clamp(sineValue, 0.1, 16)
@@ -25,30 +19,29 @@ function updatePlaybackRate() {
     });
 }
 
-let updateIntervalID = setInterval(updatePlaybackRate, settings.updateRate)
-
 chrome.runtime.onMessage.addListener(
     function (request) {
         if (request.event === "onSettingsChanged")
             chrome.storage.local.get().then(loadSettings)
-    });
+    }
+);
 
 let lastSettings = settings
 function loadSettings(newSettings) {
     settings = newSettings
 
-    if (settings.isEnabled == false && settings.isEnabled != lastSettings.isEnabled) {
+    if (lastSettings != undefined && (settings.isEnabled == false && settings.isEnabled != lastSettings.isEnabled)) {
         document.querySelectorAll('video').forEach(function (video) {
             video.playbackRate = 1
             video.preservesPitch = true
         });
     }
 
-    if (settings.updateRate != lastSettings.updateRate) {
+    if (lastSettings == undefined || settings.updateRate != lastSettings.updateRate) {
         clearInterval(updateIntervalID);
         updateIntervalID = setInterval(updatePlaybackRate, settings.updateRate)
     }
-    
+
     lastSettings = settings
 }
 
